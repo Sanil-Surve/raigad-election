b8ba2fe63764049e125d1730053c5883bfc39c4c
Sanil-Surve <sanilsurve42@gmail.com>
2024-06-04 20:00:50 +0530

    count changed

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,18 @@ const cards = [
     image:
       "https://results.eci.gov.in/uploads4/candprofile/2024/PC/E24/S13/TATKA-2024-20240423123740.jpg", // Replace with your image URL
     name: "सुनील तटकरे",
-    count: "4,77,693 (+ 69,216)",
+    count: "5,08,352 (+82,784)",
     party: "राष्ट्रवादी काँग्रेस पार्टी",
-    result: "आघाडी"
+    result: "जिंकले"
     
   },
   {
     image:
       "https://results.eci.gov.in/uploads4/candprofile/2024/PC/E24/S13/ANANT-2024-20240415060657.jpg", // Replace with your image URL
     name: "अनंत गिते",
-    count: "4,08,477 (- 69,216)",
+    count: "4,25,568 (-82,784)",
     party: "शिवसेना (उद्धव बाळासाहेब ठाकरे)",
-    result: "पिच्छाडी"
+    result: "हरले"
   },
 ];
 
@@ -25,10 +25,10 @@ const Card = ({ image, name, count, result, party}) => {
   return (
     <div className="card">
       <img src={image} alt={name} />
-      <p>{name}</p>
+      <p>नाव: {name}</p>
       <p>मतदान: {count}</p>
-      <p>{party}</p>
-      <p>{result}</p>
+      <p>पार्टी: {party}</p>
+      <p>निकाल: {result}</p>
     </div>
   );
 };
